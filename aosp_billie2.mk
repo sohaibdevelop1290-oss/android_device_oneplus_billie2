d059129fc3245547c2c34b6dc5803bc537458b5c
sohaibdevelop1290-oss <sohaibdevelop1290@gmail.com>
2026-07-04 19:36:34 +0500

    Update and rename lineage_billie2.mk to aosp_billie2.mk

--- aosp_billie2.mk
+++ aosp_billie2.mk
@@ -22,9 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/billie2/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_billie2
+PRODUCT_NAME := aosp_billie2
 PRODUCT_DEVICE := billie2
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
