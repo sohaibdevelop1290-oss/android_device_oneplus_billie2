9b38f8f2ac1786034eb185953702eceb11473b9d
sohaibdevelop1290-oss <sohaibdevelop1290@gmail.com>
2026-07-04 19:39:54 +0500

    Switch to AOSP product configuration

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_billie2.mk
+    $(LOCAL_DIR)/aosp_billie2.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_billie2-user \
-    lineage_billie2-userdebug \
-    lineage_billie2-eng
+    aosp_billie2-user \
+    aosp_billie2-userdebug \
+    aosp_billie2-eng
