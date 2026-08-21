33a6f52f2f04c8067910974ef821adecaa31f7e3
sohaibdevelop1290-oss <sohaibdevelop1290@gmail.com>
2026-08-21 15:09:38 +0500

    Enable Gapps and additional features in build
    Added flags for Gapps support and various features.

--- lineage_billie2.mk
+++ lineage_billie2.mk
@@ -24,6 +24,23 @@ $(call inherit-product, device/oneplus/billie2/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# To include Gapps 
+WITH_GMS := true
+
+# To Build Google(Dailer, Message, Phone) and BCR
+WITH_GMS_COMMS_SUITE := true
+
+# To Add cinematic wallpaer support (only supported in gapps build not in vanilla remove this flag for vanilla builds)
+TARGET_SUPPORTS_WALLEFFECT := true
+
+#Some more GMS Flag
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_STOCK_AICORE := true
+
+# To Add Bypass Charging Support (need to be adapted in DT and KT as well)
+BYPASS_CHARGE_SUPPORTED  := false
+
 PRODUCT_NAME := lineage_billie2
 PRODUCT_DEVICE := billie2
 PRODUCT_MANUFACTURER := OnePlus
